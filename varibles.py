96b32e5668653f870d4429812805be8c973b5ed9
NollOliver <NollO27@chcstudent.com>
2025-09-09 13:55:09 -0400

    Create varibles.py

---
+++ varibles.py
@@ -0,0 +1,15 @@
+x=5
+print (x)
+y = 10 
+print (x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(y**2)
+sum=x+y 
+print (sum) 
+sum= sum+20 
+print (sum)
+name = Frank the tank 
+Print (name)
+print(f"My name is {name}")
