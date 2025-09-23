025a1ae86c02c62ec2c0c4e48d0cbbd94cddbeab
NollOliver <NollO27@chcstudent.com>
2025-09-23 11:44:44 -0400

    64

---
+++ fishy.py
@@ -0,0 +1,12 @@
+fish = input("what type of fish do you want?")
+if fish == ("its carniviorous")
+if ("they already have it,")
+        print ("too bad")
+if fish == ("they dont")
+    print ("enjoy")
+elif  ("its salt water")
+    print ("you're a fancy fish parent")
+elif fish == ("community")
+    print ("you should get more than one")
+else
+    print ("i dont think thats a kind of fish")
---
+++ hailstone.py
@@ -0,0 +1,11 @@
+if "its carniviorous"
+if they already have it,
+        print ("too bad")
+if they dont
+    print ("enjoy")
+elif  its salt water
+    print ("you're a fancy fish parent")
+elif community
+    print ("you should get more than one")
+else
+    print ("i dont think thats a kind of fish")
