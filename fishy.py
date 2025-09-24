554d50d0161ef7a9b815339f608e062354c5c8af
NollOliver <NollO27@chcstudent.com>
2025-09-24 14:40:12 -0400

    fffffff

--- fishy.py
+++ fishy.py
@@ -1,12 +1,13 @@
 fish = input("what type of fish do you want?")
-if fish == ("its carniviorous")
-if ("they already have it,")
-        print ("too bad")
-if fish == ("they dont")
-    print ("enjoy")
-elif  ("its salt water")
+if fish == ("its carniviorous"):
+    option = input(" Do you already have it")
+    if option== "yes":
+        print("to Bad")
+    if option == "no":
+        print ("enjoy")
+elif fish == ("its salt water"):
     print ("you're a fancy fish parent")
-elif fish == ("community")
+elif fish == ("community"):
     print ("you should get more than one")
-else
+else:
     print ("i dont think thats a kind of fish")
---
+++ powerlevels.py
@@ -0,0 +1,9 @@
+x=input("num1")
+y=input("num2")
+x = int(x)
+y = int(y)
+if (x > y):
+    print("num1 wins")
+elif (y > x):
+    print("num2 wins")
+else : x == y
