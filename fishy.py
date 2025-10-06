8bb80c87d1c1dea36734aa04251ae0af3e52ea11
FrankDeems67 <Deemsf27@chcstudent.com>
2025-10-06 13:14:40 -0400

    e
    e

--- fishy.py
+++ fishy.py
@@ -9,5 +9,4 @@ elif fish == ("its salt water"):
     print ("you're a fancy fish parent")
 elif fish == ("community"):
     print ("you should get more than one")
-else:
-    print ("i dont think thats a kind of fish")
+
